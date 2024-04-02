9f6287fee19833d490fcfbb3dd3eb42e37f8828f
tauseef028 <touseef.afzalch@gmail.com>
2024-04-02 22:24:34 +0100

    solo final one

--- src/Constants/EligbileList.js
+++ src/Constants/EligbileList.js
@@ -4440,7 +4440,8 @@ export const eligibleList = [
     'bc1ptxkmg0zeq4g5a7hfhcc29ryplu4qzy3zrhch56p7gxvqry8vfsdqn55dm3',
     'bc1pu4g57f90gvqsv86z2qmqss4hv2puretx0mx74ge0af26qtp0dcsqx9upf3',
     'bc1p7gy0gq86snt3jpqd8ly0vh4gjmsv93t9lw75fle0uxggc9rdn8zs03cd5s',
-    
+    'bc1p6xumnwajvp7yu7mwe7txtc65lklkzwwrm3lqv4x67lp8zq0tg4ts4yglw3',
+
 
 
   ]; 
